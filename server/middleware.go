f972c83bfb8b8208e045901c84f96216262217e6
molly <email@example.com>
2023-10-04 11:17:36 +0800

    feat:check frequency

---
+++ server/middleware.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	//"fmt"
+	"github.com/gin-gonic/gin"
+	"sync"
+	"time"
+	"net/http"
+)
+
+var (
+	requestCount      int
+	lastRequestTime   time.Time
+	maxRequestsPerSecond = 5 // 允许的最大请求数
+	lock              sync.Mutex
+)
+
+func CheckFrequency() {
+	r := gin.Default()
+	r.Use(RateLimitMiddleware)
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "请求成功，总请求数：%d", requestCount)
+	})
+	r.Run(":8080")
+}
+
+func RateLimitMiddleware(c *gin.Context) {
+	lock.Lock()
+	defer lock.Unlock()
+
+	now := time.Now()
+	if now.Sub(lastRequestTime).Seconds() < 1.0/float64(maxRequestsPerSecond) {
+		c.JSON(http.StatusTooManyRequests, gin.H{"error": "请求频率过高"})
+		c.Abort()
+		return
+	}
+
+	requestCount++
+	lastRequestTime = now
+	c.Next()
+}
