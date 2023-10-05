0c1df0424c819478842dc81c253629f26d7d5c8c
molly <email@example.com>
2023-10-06 01:10:23 +0800

    feat:parser for A

---
+++ src/error.go
@@ -0,0 +1,7 @@
+package src
+
+const (
+	ERROR_HEADER_LENGTH      string = "wrong length of header"
+	ERROR_HEADER_FLAG_LENGTH string = "wrong length of flag in header"
+	ERROR_QNAME_END_MISSING  string = "QNAME parsing error: unexpected end of data"
+)
